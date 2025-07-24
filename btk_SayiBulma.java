a10f6966aa422da7153704f4062833946718624a
Suleymanov <sierra19qwe@gmail.com>
2025-07-24 16:29:20 +0000

    ufak duzeltmeler

--- btk_SayiBulma.java
+++ btk_SayiBulma.java
@@ -3,7 +3,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class btk_SayiBulma {
-    public static void Main (String[] Args) {
+    public static void main(String[] Args) {
         try (Scanner input = new Scanner(System.in)) {
             ArrayList<Integer> sayilar = new ArrayList<Integer>();
             System.out.print("Kaç sayı girmek istiyorsunuz: ");
@@ -11,23 +11,34 @@ public class btk_SayiBulma {
 
             input.nextLine();
 
-            System.out.print("Lütfen girmek istediğiniz sayıları giriniz: ");
+            System.out.println("Lütfen girmek istediğiniz sayıları giriniz: ");
+            System.out.println("");
             for (int i = 0; i < girilecekSayiMiktari; i++){
-                System.out.println((i + 1) + ". sayıyı giriniz");
+                System.out.print((i + 1) + ". sayıyı giriniz: ");
                 int sayi = input.nextInt();
                 sayilar.add(sayi);
+                System.out.println("");
             }
             input.nextLine();
             System.out.println("Girdi alma işlemi tamamlandı! ");
             System.out.print("Lütfen bulmak istediğiniz sayıyı giriniz: ");
             int arananSayi = input.nextInt();
 
+            boolean sayiBulunduMu = false;
             for (int i:sayilar){
                 if (i == arananSayi) {
-                    System.out.println("Aradağınız sayı listede bulunuyor!");
-                }else {
-                    System.out.println("Aradağınız sayı listede bulunmuyor!");
+                    sayiBulunduMu = true;
+                    break;
                 }
+            }
+            if (sayiBulunduMu) {
+                System.out.println("-----------------------------------");
+                System.out.println("Aradağınız sayı listede bulunuyor!");
+                System.out.println("-----------------------------------");
+            }else {
+                System.out.println("-----------------------------------");
+                System.out.println("Aradağınız sayı listede bulunmuyor!");
+                System.out.println("-----------------------------------");
             }
         }
         catch (InputMismatchException e) {
